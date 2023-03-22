34e915dd7ffbccf64fb004050d72a51517c2c0f2
Bauyrzhan Abdi <bauyrzhan.abdi@nu.edu.kz>
2023-03-22 16:11:36 +0600

    set up tabbar

---
+++ PhotosApp/Source/ViewController/AlbumsViewController.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+final class AlbumsViewController: UIViewController {
+    
+    // MARK: - Outlets
+    
+    
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    // MARK: - Setup
+}
---
+++ PhotosApp/Source/ViewController/SearchViewController.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+final class SearchViewController: UIViewController {
+    
+    // MARK: - Outlets
+    
+    
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    // MARK: - Setup
+}
